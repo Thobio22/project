376d8245c6a69c378ca567c925f76173f772df16
Thobio22 <thomas.verouden@hotmail.com>
2019-06-26 02:11:27 +0200

    update2

--- project.js
+++ project.js
@@ -47,22 +47,22 @@ window.onload = function() {
     slider_output.innerHTML = year;
 
 
-    //get elements from dropdown (link source in code description)
-    var dropdownTitle = document.querySelector('.dropdown .title');
-    var dropdownOptions = document.querySelectorAll('.dropdown .option');
-
-
-    //bind listeners to dropdown elements gotten (link source in code description)
-    dropdownTitle.addEventListener('click', toggleMenuDisplay);
-    dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
-    document.querySelector('.dropdown .title').addEventListener('change', handleTitleChange); // changes periodvar html variable
-    document.querySelector('.dropdown .title').addEventListener('change', function(){periodUpdate(data_cause)}); // gives data needed in updatePie
+    // //get elements from dropdown (link source in code description)
+    // var dropdownTitle = document.querySelector('.dropdown .title');
+    // var dropdownOptions = document.querySelectorAll('.dropdown .option');
+    //
+    //
+    // //bind listeners to dropdown elements gotten (link source in code description)
+    // dropdownTitle.addEventListener('click', toggleMenuDisplay);
+    // dropdownOptions.forEach(option => option.addEventListener('click',handleOptionSelected));
+    // document.querySelector('.dropdown .title').addEventListener('change', handleTitleChange); // changes periodvar html variable
+    // document.querySelector('.dropdown .title').addEventListener('change', function(){periodUpdate(data_cause)}); // gives data needed in updatePie
 
 
     // act when default button has been pressed
-    d3v5.select("#defaultButton").on("click", function(){getButton(data_cause, data_bee_line, data_crop)});
+    d3v5.select("#defaultButton").on("click", function(){getButton()});
 
-    function getButton(data_cause) {
+    function getButton(data_cause, data_bee_line, data_crop) {
       // set selected html variable to desired value
       document.getElementById("periodvar").value = "year";
       document.getElementById("statevar").value = "TO"
@@ -70,9 +70,9 @@ window.onload = function() {
       // get html variables
       var selectedYear = document.getElementById("yearvar").value
       var selectedState = document.getElementById("statevar").value
-      var selectedPeriod = document.getElementById("periodvar").value
-      console.log(selectedPeriod)
-      // updatePie(selectedYear, selectedState, selectedPeriod);
+      // var selectedPeriod = document.getElementById("periodvar").value
+      // console.log(selectedPeriod)
+      updatePie(selectedYear, selectedState);
       // updateLine(selectedState);
     };
 
@@ -590,8 +590,9 @@ function drawLinechart(data_crop, data_bee_line, state) {
        // .on('mouseout', beelineTip.hide);
 
 
+  // Add crop production line
   svg.append("path")
-       .attr("class", "beeLine")
+       .attr("class", "cropLine")
        .attr("stroke", "blue")
        .attr("fill", "none")
        .attr("d", lineCrop(data_crop[state]))
@@ -651,14 +652,66 @@ function drawLinechart(data_crop, data_bee_line, state) {
        .text("Yield in Kg per Acre");
 
 
-  function updateLinechart() {
+  drawLinechart.updateLine = updateLine;
 
-    console.log("komt in update line, state: ", state)
+  function updateLine() {
 
-    // get html variables for updating figures
-    var selectedYear = document.getElementById("yearvar").value;
-    var selectedState = document.getElementById("statevar").value;
-    var selectedPeriod = document.getElementById("periodvar").value;
+      console.log("komt in update line, state: ", state)
+
+      // get html variables for updating figures
+      var selectedYear = document.getElementById("yearvar").value;
+      var selectedState = document.getElementById("statevar").value;
+      var selectedPeriod = document.getElementById("periodvar").value;
+
+      var lineDim = {
+                    top: 20,
+                    right: 60,
+                    left: 60,
+                    bottom: 25,
+                    width: 480,
+                    height: 400
+                    };
+
+      // // create x, y scaling for placing data in svg pixels
+      // var xScale = d3v5.scaleTime()
+      //                  .domain([
+      //                    d3v5.min(data_bee_line[state], function (d) {return d["Year"] }),
+      //                    d3v5.max(data_bee_line[state], function (d) {return d["Year"]})])
+      //                  .range([lineDim.top, lineDim.width]);
+      //
+      //
+      // var yScaleBee = d3v5.scaleLinear()
+      //                     .domain([0, 100])
+      //                     .range([lineDim.height - lineDim.top, lineDim.top]);
+      //
+      //
+      // var yScaleCrop = d3v5.scaleLinear()
+      //                  .domain([
+      //                    d3v5.min(data_crop[state], function (d) {return d["Kg_per_Acre"] }) - (d3v5.min(data_crop[state], function (d) {return d["Kg_per_Acre"] })/2),
+      //                    d3v5.max(data_crop[state], function (d) {return d["Kg_per_Acre"] }) + (d3v5.max(data_crop[state], function (d) {return d["Kg_per_Acre"] })/2)])
+      //                  .range([lineDim.height - lineDim.top, lineDim.top]);
+
+      // Add the bee loss line
+      var lineBee = d3v5.line()
+                 .x(function(d) {
+                   return xScale(d["Year"]);
+                 })
+                 .y(function(d) {
+                   return yScaleBee(d["Total_Annual_Loss"]);
+                 });
+
+      var lineCrop = d3v5.line()
+                 .x(function(d) {
+                   return xScale(d["Year"]);
+                 })
+                 .y(function(d) {
+                   // console.log(d)
+                   return yScaleCrop(d["Kg_per_Acre"]);
+                 });
+
+
+
+
 
 
   }
@@ -666,70 +719,70 @@ function drawLinechart(data_crop, data_bee_line, state) {
 };
 
 
-function toggleClass(elem,className){
-	if (elem.className.indexOf(className) !== -1) {
-		elem.className = elem.className.replace(className,'');
-	}
-	else {
-		elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
-	};
-
-	return elem;
-};
-
-
-function toggleDisplay(elem) {
-	var curDisplayStyle = elem.style.display;
-
-	if (curDisplayStyle === 'none' || curDisplayStyle === ''){
-		elem.style.display = 'block';
-	}
-	else{
-		elem.style.display = 'none';
-	}
-};
-
-
-function toggleMenuDisplay(e) {
-	var dropdown = e.currentTarget.parentNode;
-	var menu = dropdown.querySelector('.menu');
-	var icon = dropdown.querySelector('.fa-angle-right');
-
-	toggleClass(menu,'hide');
-	toggleClass(icon,'rotate-90');
-};
-
-
-function handleOptionSelected(e) {
-	toggleClass(e.target.parentNode, 'hide');
-
-	var id = e.target.id;
-	var newValue = e.target.textContent + ' ';
-	var titleElem = document.querySelector('.dropdown .title');
-	var icon = document.querySelector('.dropdown .title .fa');
-
-
-	titleElem.textContent = newValue;
-	titleElem.appendChild(icon);
-
-	//trigger custom event
-	document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
-	//setTimeout is used so transition is properly shown
-	setTimeout(() => toggleClass(icon,'rotate-90',0));
-};
-
-
-function handleTitleChange(e) {
-    // set selected html variable to desired value
-    document.getElementById("periodvar").value = e.target.innerText;
-};
-
-
-function periodUpdate(data_cause) {
-  // call in all needed variables
-  var selectedYear = document.getElementById("yearvar").value
-  var selectedState = document.getElementById("statevar").value
-  var selectedPeriod = document.getElementById("periodvar").value
-  console.log(selectedPeriod)
-  drawPiechart.updatePie();
-};
+// function toggleClass(elem,className){
+// 	if (elem.className.indexOf(className) !== -1) {
+// 		elem.className = elem.className.replace(className,'');
+// 	}
+// 	else {
+// 		elem.className = elem.className.replace(/\s+/g,' ') + 	' ' + className;
+// 	};
+//
+// 	return elem;
+// };
+//
+//
+// function toggleDisplay(elem) {
+// 	var curDisplayStyle = elem.style.display;
+//
+// 	if (curDisplayStyle === 'none' || curDisplayStyle === ''){
+// 		elem.style.display = 'block';
+// 	}
+// 	else{
+// 		elem.style.display = 'none';
+// 	}
+// };
+//
+//
+// function toggleMenuDisplay(e) {
+// 	var dropdown = e.currentTarget.parentNode;
+// 	var menu = dropdown.querySelector('.menu');
+// 	var icon = dropdown.querySelector('.fa-angle-right');
+//
+// 	toggleClass(menu,'hide');
+// 	toggleClass(icon,'rotate-90');
+// };
+//
+//
+// function handleOptionSelected(e) {
+// 	toggleClass(e.target.parentNode, 'hide');
+//
+// 	var id = e.target.id;
+// 	var newValue = e.target.textContent + ' ';
+// 	var titleElem = document.querySelector('.dropdown .title');
+// 	var icon = document.querySelector('.dropdown .title .fa');
+//
+//
+// 	titleElem.textContent = newValue;
+// 	titleElem.appendChild(icon);
+//
+// 	//trigger custom event
+// 	document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
+// 	//setTimeout is used so transition is properly shown
+// 	setTimeout(() => toggleClass(icon,'rotate-90',0));
+// };
+//
+//
+// function handleTitleChange(e) {
+//     // set selected html variable to desired value
+//     document.getElementById("periodvar").value = e.target.innerText;
+// };
+//
+//
+// function periodUpdate(data_cause) {
+//   // call in all needed variables
+//   var selectedYear = document.getElementById("yearvar").value
+//   var selectedState = document.getElementById("statevar").value
+//   var selectedPeriod = document.getElementById("periodvar").value
+//   console.log(selectedPeriod)
+//   drawPiechart.updatePie();
+// };
